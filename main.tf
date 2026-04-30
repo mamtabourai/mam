312945b4ceb69b22dd9463ec27683c16d3c5702f
Mamta <mamta.bourai2107@gmail.com>
2026-04-30 17:39:27 +0530

    udated storage account and container infra

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 resource "azurerm_resource_group" "anna" {
-  for_each = tomap(var.varsha)
+  for_each = var.varsha
   name     = each.value.name
   location = each.value.location
 }
 
 resource "azurerm_storage_account" "banna" {
-  for_each                = tomap(var.varsha)
-  name                    = "strg00${each.key}"
-  resource_group_name     = azurerm_resource_group.anna[each.key].name
-  location                = azurerm_resource_group.anna[each.key].location
+  for_each                 = var.varsha
+  name                     = each.value.storage_name
+  resource_group_name      = azurerm_resource_group.anna[each.key].name
+  location                 = azurerm_resource_group.anna[each.key].location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
 
 resource "azurerm_storage_container" "cbanna" {
-  for_each                = tomap(var.varsha)
-  name                    = "cont00${each.key}"
-  storage_account_id = azurerm_storage_account.banna[each.key].id
+  for_each              = var.varsha
+  name                  = each.value.container_name
+  storage_account_id    = azurerm_storage_account.banna[each.key].id
   container_access_type = "private"
 
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,18 +1,26 @@
 varsha = {
   rg1 = {
-    name     = "anna"
-    location = "east us"
+    name           = "anna"
+    location       = "east us"
+    storage_name   = "annastorage"
+    container_name = "annacontainer"
   }
   rg2 = {
-    name     = "banna"
-    location = "west us"
+    name           = "banna"
+    location       = "west us"
+    storage_name   = "bannastorage"
+    container_name = "bannacontainer"
   }
   rg3 = {
-    name     = "lanna"
-    location = "south india"
+    name           = "lanna"
+    location       = "south india"
+    storage_name   = "lannastorage"
+    container_name = "lannacontainer"
   }
   rg4 = {
-    name     = "ganna"
-    location = "central us"
+    name           = "ganna"
+    location       = "central us"
+    storage_name   = "gannastorage"
+    container_name = "gannacontainer"
   }
 }
